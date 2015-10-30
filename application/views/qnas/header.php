da3278db8b3b639fcb5ed786fa0cb46b637a61e1
rikardote <ricardofa1980@gmail.com>
2015-10-30 06:26:27 +0000

    first commit

--- application/views/qnas/header.php
+++ application/views/qnas/header.php
@@ -9,22 +9,30 @@
 		<tr>
 			<td><img alt="" src="<?=base_url()?>assets/images/issste.png" style="width: 400px; height: 108px;" /></td>
 			<td align='right'>
-			<p><strong>DELEGACION ESTATAL BAJA CALIFORNIA</strong></p>
+			<p><h2><strong>DELEGACION ESTATAL BAJA CALIFORNIA</strong></h2></p>
 
-			<p><strong>SUBDELEGACION DE ADMINISTRACION</strong></p>
+			<p><h3><strong>SUBDELEGACION DE ADMINISTRACION</strong></h3></p>
 
-			<p><strong>DEPARTAMENTO DE RECURSOS HUMANOS</strong></p>
-			<p>&nbsp;</p>
-			<p>&nbsp;</p>
+			<p><h4><strong>DEPARTAMENTO DE RECURSOS HUMANOS</strong></h4></p>
+			
+			
 			</td>
 		</tr>
+		<table border="0" cellpadding="1" cellspacing="1" style="width:500px;">
+			<tbody>
+				<tr>
+					<p><td style="width: 100%; background-color: rgb(102, 102, 102);"><span style="color:#FFFFFF;">REPORTE DE CONTROL DE ASISTENCIA</span></td></p>
+				</tr>
+			</tbody>
+		</table>
+
 		<tr>
-			<td><span style="font-size:14px;">CLAVE DE ADSCRIPCION: <strong><?=$reporte2->adscripcion?></strong></span></td>
-			<td align='right'><span style="font-size:14px;">DESCRIPCION: <strong><?=$reporte2->descripcion?></strong></span></td>
+			<td><span style="font-size:16px;">CLAVE DE ADSCRIPCION: <strong><?=$reporte2->adscripcion?></strong></span></td>
+			<td align='right'><span style="font-size:16px;">DESCRIPCION: <strong><?=$reporte2->descripcion?></strong></span></td>
 		</tr>
 		<tr>
-			<td><span style="font-size:14px;">QNA: <strong><?php echo $reporte2->qna_mes.'/'.$reporte2->qna_year.' - '.$reporte2->qna_descripcion;?>  </strong></span></td>
-			<td align='right'><span style="font-size:14px;">A&Ntilde;O: <strong><?=$reporte2->qna_year?></strong></span></td>
+			<td><span style="font-size:16px;">QNA: <strong><?php echo $reporte2->qna_mes.'/'.$reporte2->qna_year.' - '.$reporte2->qna_descripcion;?>  </strong></span></td>
+			<td align='right'><span style="font-size:16px;">A&Ntilde;O: <strong><?=$reporte2->qna_year?></strong></span></td>
 		</tr>
 	</tbody>
 </table>
